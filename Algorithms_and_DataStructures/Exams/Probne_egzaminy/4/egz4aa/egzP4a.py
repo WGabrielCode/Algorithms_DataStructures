43bfc1e93fb8580f9de892bb96637aa47007d60b
WGabrielCode <gwerminski@student.agh.edu.pl>
2025-09-04 13:59:47 +0200

    solved exam 4

--- Algorithms_and_DataStructures/Exams/Probne_egzaminy/4/egz4aa/egzP4a.py
+++ Algorithms_and_DataStructures/Exams/Probne_egzaminy/4/egz4aa/egzP4a.py
@@ -1,7 +1,60 @@
 from egzP4atesty import runtests 
 
-def mosty ( T ):
-    #tutaj proszę wpisać własną implementację 
-    return 0
+#O( n^2 ) runtime error
+def mosty_2 ( T ):
 
-runtests ( mosty, all_tests=False )
+    T.sort(  )
+    n = len( T )
+    dp = [1] * n
+
+    for i in range( 1 , n ) :
+        for j in range( i ) :
+            if T[i][1] > T[j][1] :
+                dp[i] = max( dp[i] , dp[j]+1 )
+
+    return max( dp )
+"""
+Orientacyjny łączny czas : 0.26 sek.
+Status testów: A A A A A A A A A A
+O(nlogn)
+"""
+def mosty ( T ):
+
+    T.sort()
+    n = len( T )
+
+    result = []
+
+    def search( x ) :
+        left = 0
+        right = len( result )
+
+        while left <= right :
+
+            if left == right  :
+                return left
+
+            mid = (left+right) // 2
+            if result[mid] <= x :
+                if result[mid] == x :
+                    return mid
+                left = mid + 1
+            else :
+                right = mid
+
+    for i in range( n ) :
+        val = T[i][1]
+        idx = search( val )
+        if idx == len( result ) :
+            result.append( val )
+        else :
+            result[idx] = val
+
+    return len( result )
+
+"""
+T = [ (1, 2), (2, 3), (3, 0) ,(1,0),(10,100),(12,12) ]
+print( mosty( T ) )
+"""
+
+runtests ( mosty, all_tests=True  )
--- Algorithms_and_DataStructures/Exams/Probne_egzaminy/4/egzp4b/egzP4b.py
+++ Algorithms_and_DataStructures/Exams/Probne_egzaminy/4/egzp4b/egzP4b.py
@@ -1,16 +1,61 @@
-from egzP4btesty import runtests 
+from egzP4btesty import runtests
+"""
+Orientacyjny łączny czas : 0.34 sek.
+Status testów: A A A A A A A T T T
+O( n+q )  
+"""
 
-class Node:
-  def __init__(self, key, parent):
-    self.left = None
-    self.right = None
-    self.parent = parent
-    self.key = key
-    self.x = None
+class Node :
+	def __init__( self, key, parent ) :
+		self.left = None
+		self.right = None
+		self.parent = parent
+		self.key = key
+		self.x = None
 
+def sol( root, T ) :
 
-def sol(root, T):
+	head = root
+	arr = []
+	def emp( head ) :
 
-    return 0
-    
+		if head.left :
+			emp( head.left )
+		arr.append( head.key )
+		if head.right :
+			emp( head.right )
+
+	emp( head )
+	memo = {}
+	for w in T :
+		memo[w.key] = 0
+
+	result = 0
+	for i in range( 1 , len( arr)-1 ) :
+		if arr[i] in memo and (arr[i-1]+arr[i+1])/2 == arr[i] :
+			result += arr[i]
+
+	return result
+
+"""
+w11 = Node( 11, None )
+w5 = Node( 5, w11 )
+w11.left = w5
+w15 = Node( 15, w11 )
+w11.right = w15
+w3 = Node( 3, w5 )
+w5.left = w3
+w8 = Node( 8, w5 )
+w5.right = w8
+w12 = Node( 12, w15 )
+w15.left = w12
+w7 = Node( 7, w8 )
+w8.left = w7
+w10 = Node( 10, w8 )
+w8.right = w10
+T = [ w5, w7, w8, w11, w12 ]
+
+print( sol( w11, T ) )
+"""
+
 runtests(sol, all_tests = True)

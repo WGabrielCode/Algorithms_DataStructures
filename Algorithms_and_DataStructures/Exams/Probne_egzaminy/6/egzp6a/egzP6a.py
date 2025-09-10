1400fb81ef425ded368c235789d70c6920357688
WGabrielCode <gwerminski@student.agh.edu.pl>
2025-09-10 13:25:59 +0200

    egzP6a solved

--- Algorithms_and_DataStructures/Exams/Probne_egzaminy/6/egzp6a/egzP6a.py
+++ Algorithms_and_DataStructures/Exams/Probne_egzaminy/6/egzp6a/egzP6a.py
@@ -1,8 +1,72 @@
-from egzP6atesty import runtests 
+from egzP6atesty import runtests
 
-def google ( H, s ):
-    #tutaj proszę wpisać własną implementację
-    return ""
+"""
+Orientacyjny łączny czas : 0.37 sek.
+Status testów: A A A A A A A T T T
+O(nk + nlogn)
+"""
+def google( H, s ) :
+	memo = {}
+	for h in H :
+		cnt = 0
+		for char in h :
+			if "0" <= char <= "9" :
+				cnt += 1
+		memo[ h ] = ( len(h)-cnt )
+
+	def merge( left , mid , right ) :
+		T[left:right+1] = H[left:right+1]
+		i = left
+		j = mid+1
+		k = left
+		while i <= mid and j <= right :
+			if len( T[i] ) > len( T[j] ) :
+				H[ k ] = T[ i ]
+				k += 1
+				i += 1
+			elif len( T[i] ) == len( T[j] ) :
+				if memo[T[i]] > memo[T[j]] :
+					H[k] = T[i]
+					k += 1
+					i += 1
+				else :
+					H[k] = T[j]
+					j += 1
+					k += 1
+			else :
+				H[ k ] = T[ j ]
+				j += 1
+				k += 1
 
 
-runtests ( google, all_tests=False )
+
+		while i <= mid :
+			H[k] = T[i]
+			i += 1
+			k += 1
+		while j <= right :
+			H[k] = T[j]
+			k += 1
+			j += 1
+
+	def merge_sort(left ,right ) :
+		if left < right :
+			mid = (left+right)//2
+			merge_sort( left , mid )
+			merge_sort( mid+1 , right )
+			merge( left , mid , right )
+
+	n = len(H)
+	T = [0] * n
+
+	merge_sort( 0 , n-1 )
+
+	return H[s-1]
+
+#"""
+H = [ 'aba', 'abc', 'ab1', 'abab', 'a1a1', 'aa12a']
+s = 3
+print( google( H, s ) )
+#"""
+
+#runtests ( google, all_tests=True  )
